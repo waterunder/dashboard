edb497626fd2f07c429d8f4148384bc1c8a03316
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-03 18:10:49 -0300

    added pages app to urls

--- main/urls.py
+++ main/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('dashboard.urls')),
+    path('', include('pages.urls')),
+    path('dashboard/', include('dashboard.urls')),
 ]
