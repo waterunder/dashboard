9f6469bc72b002193bf60ddc9da89694527600ef
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-03 18:12:17 -0300

    changed function view to class based view

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from . import views
+from .views import DashboardPageView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', DashboardPageView.as_view(), name='dashboard'),
 ]
