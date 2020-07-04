58433335172a66d084ce46dc860b3f5fe6b7e280
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-04 14:22:52 -0300

    added accounts endpoint to handle login

--- main/urls.py
+++ main/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('pages.urls')),
     path('dashboard/', include('dashboard.urls')),
 ]
