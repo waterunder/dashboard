6865b098220d4048e579bf002110d7628c104aca
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-04 16:14:07 -0300

    unit tests for about page

--- pages/tests.py
+++ pages/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve, reverse
 
-from .views import HomePageView
+from .views import AboutPageView, HomePageView
 
 
 class HomePageTests(TestCase):
@@ -15,3 +15,16 @@ class HomePageTests(TestCase):
     def test_home_page_resolves_homepageview(self):
         view = resolve('/')
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+
+
+class AboutPageTests(TestCase):
+    def test_aboutpage_works(self):
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about.html')
+        self.assertContains(response, 'About')
+        self.assertNotContains(response, 'Hi I should be on this page!')
+
+    def test_about_page_resolves_aboutpageview(self):
+        view = resolve('/about/')
+        self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
