24751394d569f8d2ace3e06efd72d33a1b4445c9
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-21 22:00:53 -0300

    added products admin to view all fields

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'price',)
+
+
+admin.site.register(Product, ProductAdmin)
