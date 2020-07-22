d15884058f5ea6210fdb66f14e9e75b099556b13
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-22 19:05:26 -0300

    added image field to product model

--- products/models.py
+++ products/models.py
@@ -10,6 +10,7 @@ class Product(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    cover = models.ImageField(upload_to='covers/', blank=True)
 
     def __str__(self):
         return self.title
