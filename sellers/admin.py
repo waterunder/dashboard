8c82152f5657b1fe46451bfcf8882b78ad6aa3a0
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-23 19:24:00 -0300

    added product as an tabular inline to seller

--- sellers/admin.py
+++ sellers/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
+from products.models import Product
+
 from .models import Seller
 
 
+class ProductInline(admin.TabularInline):
+    model = Product
+
+
 class SellerAdmin(admin.ModelAdmin):
+    inlines = (ProductInline, )
     list_display = ('name', 'email', 'country', 'is_active', 'owner',)
 
 
