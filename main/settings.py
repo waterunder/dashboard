95ed764f292825441e4330ef3323a779323b8679
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-23 18:58:52 -0300

    added 0.0.0.0 to allowed hosts

--- main/settings.py
+++ main/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['immense-inlet-32216.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['immense-inlet-32216.herokuapp.com', 'localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
