5349ba9cbe60b9e00a7bf65914f83e4f9d81fb46
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-23 15:45:52 -0300

    added list and detail view for each seller

--- sellers/views.py
+++ sellers/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
-# Create your views here.
+from .models import Seller
+
+
+class SellerListView(ListView):
+    model = Seller
+    context_object_name = 'seller_list'
+    template = 'sellers/seller_list.html'
+
+
+class SellerDetailView(DetailView):
+    model = Seller
+    context_object_name = 'seller'
+    template = 'sellers/seller_detail.html'
