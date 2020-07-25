42a8332a4122f1b398f7d96e3be99d1bcb00fbfe
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-25 19:14:25 -0300

    added productimage to admin interface

--- products/admin.py
+++ products/admin.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 
-from .models import Product, Review
+from .models import Product, ProductImage, ProductTag, Review
 
 
 class ReviewInline(admin.TabularInline):
     model = Review
 
 
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+
+
 class ProductAdmin(admin.ModelAdmin):
-    inlines = [ReviewInline, ]
+    inlines = (ProductImageInline, ReviewInline, )
     list_display = ('title', 'description', 'price',)
 
 
