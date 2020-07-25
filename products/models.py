84ea149b87c2ae05d40f3ce124cec5505abbd396
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-24 22:24:20 -0300

    added thumbnail option for product image

--- products/models.py
+++ products/models.py
@@ -12,8 +12,9 @@ class Product(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    cover = models.ImageField(upload_to='covers/', blank=True)
-
+    active = models.BooleanField(default=True)
+    in_stock = models.BooleanField(default=True)
+    date_updated = models.DateTimeField(auto_now=True)
     seller = models.ForeignKey(Seller, on_delete=models.CASCADE, related_name='products',)
 
     def __str__(self):
@@ -21,6 +22,20 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('product_detail', args=[str(self.id)])
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product-images')
+    thumbnail = models.ImageField(upload_to='product-thumbnails', null=True)
+
+
+class ProductTag(models.Model):
+    products = models.ManyToManyField(Product, blank=True)
+    name = models.CharField(max_length=32)
+    slug = models.SlugField(max_length=48)
+    description = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
 
 
 class Review(models.Model):
