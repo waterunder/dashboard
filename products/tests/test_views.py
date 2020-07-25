307105308a7322afbe5e9b8de11da35ae2ae30fa
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-25 18:51:55 -0300

    added reference for foreign key

--- products/tests/test_views.py
+++ products/tests/test_views.py
@@ -2,7 +2,8 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from .models import Product, Review
+from products.models import Product, Review
+from sellers.models import Seller
 
 
 class ProductTests(TestCase):
@@ -12,9 +13,21 @@ class ProductTests(TestCase):
             email='reviewuser@email.com',
             password='testpass123',
         )
-
+        self.seller = Seller.objects.create(
+            name='Test Company LLC',
+            description='Top worldwide experts in selling',
+            email='testcompany@email.com',
+            address1='Av de Mayo 855, portaldelsur, CABA',
+            zip_code='1086',
+            city='Buenos Aires',
+            country='uk',
+            owner=self.user,
+        )
         self.product = Product.objects.create(
-            title='Kepler155B', description='The first exoplanet found', price='250.00',)
+            title='Kepler155B',
+            description='The first exoplanet found',
+            price='250.00',
+            seller=self.seller)
 
         self.review = Review.objects.create(
             product=self.product,
@@ -22,18 +35,18 @@ class ProductTests(TestCase):
             author=self.user,
         )
 
-    def test_book_listing(self):
+    def test_product_listing(self):
         self.assertEqual(f'{self.product.title}', 'Kepler155B')
         self.assertEqual(f'{self.product.description}', 'The first exoplanet found')
         self.assertEqual(f'{self.product.price}', '250.00')
 
-    def test_book_list_view(self):
+    def test_product_list_view(self):
         response = self.client.get(reverse('product_list'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Kepler')
         self.assertTemplateUsed(response, 'products/product_list.html')
 
-    def test_book_detail_view(self):
+    def test_product_detail_view(self):
         response = self.client.get(self.product.get_absolute_url())
         no_response = self.client.get('/products/1234/')
 
