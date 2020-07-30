ab2a8309687ad95b49e1651eae2062aa40e6d065
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-30 18:23:05 -0300

    upcoming fixtures with better rendering

--- products/fixtures/generate.py
+++ products/fixtures/generate.py
@@ -7,17 +7,23 @@ fake = Faker()
 
 def generate():
     return {
-        "model": "products.producttag",
+        "model": "products.product",
+        "pk": fake.uuid4(),
         "fields": {
-            "name": fake.word(),
-            "slug": fake.slug(),
+            "title": fake.word(),
             "description": fake.paragraph(),
-            "active": fake.boolean()
+            "price": str(fake.random_int()),
+            "active": fake.boolean(),
+            "in_stock": fake.boolean(),
+            "date_updated": "2020-07-30T21:08:20.119Z",
+            "seller": "9db3c6f9-fd3e-4490-9529-b97c205ef866",
+            "tags": [
+            ]
         }
     }
 
 
-data = [generate() for i in range(50)]
+data = [generate() for _ in range(50)]
 
-with open('products/fixtures/producttags.json', 'w') as f:
+with open('products/fixtures/products.json', 'w') as f:
     f.write(json.dumps(data))
