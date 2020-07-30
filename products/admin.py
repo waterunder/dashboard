d2e9fea303c6bf4fb20406bf570c2fe20313bcb1
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-30 16:38:54 -0300

    added product tag to admin site

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Product, ProductImage, ProductTag, Review
 
@@ -13,7 +14,40 @@ class ProductImageInline(admin.TabularInline):
 
 class ProductAdmin(admin.ModelAdmin):
     inlines = (ProductImageInline, ReviewInline, )
-    list_display = ('title', 'description', 'price',)
+    list_display = ('title', 'price', 'in_stock', 'seller')
+    list_filter = ('price', 'in_stock', 'date_updated',)
+    list_editable = ('in_stock',)
+    search_fields = ('name',)
+    autocomplete_fields = ('tags',)
+
+
+class ProductTagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'active')
+    list_editable = ('active',)
+    list_filter = ('active',)
+    search_fields = ('name',)
+    prepopulated_fields = {"slug": ("name",)}
 
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductTag, ProductTagAdmin)
+
+
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ('thumbnail_tag', 'product_name')
+    readonly_fields = ('thumbnail',)
+    search_fields = ('product__name',)
+
+    def thumbnail_tag(self, obj):
+        if obj.thumbnail:
+            return format_html(
+                '<img src="%s"/>' % obj.thumbnail.url
+            )
+        return "-"
+    thumbnail_tag.short_description = 'Thumbnail'
+
+    def product_name(self, obj):
+        return obj.product.name
+
+
+admin.site.register(ProductImage, ProductImageAdmin)
