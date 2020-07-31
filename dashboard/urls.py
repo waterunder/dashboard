4dbd1cb7d0fa39c404c4b94a9c9db10496abc71c
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-31 16:20:48 -0300

    rewired new endpoints for the dashboard app

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
-from .views import DashboardPageView
+from . import views
 
 urlpatterns = [
-    path('', DashboardPageView.as_view(), name='dashboard'),
+    path('', views.DashboardPageView.as_view(), name='dashboard'),
+    path('icons/', views.IconsPageView.as_view(), name='icons'),
+    path('forms/', views.FormsPageView.as_view(), name='forms'),
+    path('tables/', views.TablesPageView.as_view(), name='tables'),
+    path('calendar/', views.CalendarPageView.as_view(), name='calendar'),
+    path('profile/', views.ProfilePageView.as_view(), name='profile'),
+    path('login/', views.LoginPageView.as_view(), name='login'),
+    path('register/', views.RegisterPageView.as_view(), name='register'),
 ]
