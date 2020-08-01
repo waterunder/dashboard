4097e3ae8be4db3ab3f609c2e764399b21537d1c
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-01 11:06:37 -0300

    updated broken links in test

--- pages/tests/test_views.py
+++ pages/tests/test_views.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import resolve, reverse
 
 from pages import forms
-from pages.views import AboutPageView, HomePageView, ContactUsView
+from pages.views import AboutPageView, ContactUsView, HomePageView
 
 
 class HomePageTests(TestCase):
@@ -14,7 +14,7 @@ class HomePageTests(TestCase):
         self.assertNotContains(response, 'Hi there! I should not be here')
 
     def test_home_page_resolves_homepageview(self):
-        view = resolve('/')
+        view = resolve('/pages/')
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
 
 
@@ -27,7 +27,7 @@ class AboutPageTests(TestCase):
         self.assertNotContains(response, 'Hi I should be on this page!')
 
     def test_about_page_resolves_aboutpageview(self):
-        view = resolve('/about/')
+        view = resolve('/pages/about/')
         self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
 
 
@@ -41,5 +41,5 @@ class ContactPageTests(TestCase):
         self.assertIsInstance(response.context['form'], forms.ContactForm)
 
     def test_contact_us_page_resolve_contactpageview(self):
-        view = resolve('/contact-us/')
+        view = resolve('/pages/contact-us/')
         self.assertEqual(view.func.__name__, ContactUsView.as_view().__name__)
