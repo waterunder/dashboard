5f4b4933a03f6030ac344ff292632a71068cd10c
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-03 19:05:02 -0300

    using templates from the app itself to render the views

--- products/views.py
+++ products/views.py
@@ -6,10 +6,10 @@ from .models import Product
 class ProductListView(ListView):
     model = Product
     context_object_name = 'product_list'
-    template_name = 'dashboard/product_list.html'
+    template_name = 'products/product_list.html'
 
 
 class ProductDetailView(DetailView):
     model = Product
     context_object_name = 'product'
-    template_name = 'dashboard/product_detail.html'
+    template_name = 'products/product_detail.html'
