6c596cc38009506a3f9578953666a23cc9aaa753
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-03 13:31:59 -0300

    changed product detail template

--- products/views.py
+++ products/views.py
@@ -12,4 +12,4 @@ class ProductListView(ListView):
 class ProductDetailView(DetailView):
     model = Product
     context_object_name = 'product'
-    template_name = 'products/product_detail.html'
+    template_name = 'dashboard/product_detail.html'
