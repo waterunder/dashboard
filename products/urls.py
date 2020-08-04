ba55c7d0307adcdfb3c0fd28f955ea2966d946a4
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-04 15:27:05 -0300

    added SellerProductView

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ProductDetailView, ProductListView
+from .views import ProductDetailView, ProductListView, SellerProductList
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
-    path('<uuid:pk>', ProductDetailView.as_view(), name='product_detail')
+    path('<uuid:pk>', ProductDetailView.as_view(), name='product_detail'),
+    path('<seller>', SellerProductList.as_view(), name='seller_product_list'),
 ]
