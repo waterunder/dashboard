002c444a19790a7a43900a006890f562d1ebab58
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-04 15:27:11 -0300

    added SellerProductView

--- products/views.py
+++ products/views.py
@@ -1,6 +1,12 @@
+import logging
+
+from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView, ListView
 
-from .models import Product
+from products.models import Product
+from sellers.models import Seller
+
+logger = logging.getLogger(__name__)
 
 
 class ProductListView(ListView):
@@ -13,3 +19,18 @@ class ProductDetailView(DetailView):
     model = Product
     context_object_name = 'product'
     template_name = 'products/product_detail.html'
+
+
+class SellerProductList(ListView):
+    template_name = 'products/products_by_seller.html'
+
+    def get_queryset(self):
+        self.seller = get_object_or_404(Seller, name=self.kwargs['seller'])
+        return Product.objects.filter(seller=self.seller)
+
+    def get_context_data(self, **kwargs):
+        # call the base context first
+        context = super().get_context_data(**kwargs)
+        context['seller'] = self.seller
+        logger.info('context: %s', context)
+        return context
