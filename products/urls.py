25c9032b906719a6de24ece6c28d579ee0892814
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-04 16:08:04 -0300

    added new endpoint for products by a seller

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,6 @@ from .views import ProductDetailView, ProductListView, SellerProductList
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
-    path('<uuid:pk>', ProductDetailView.as_view(), name='product_detail'),
-    path('<seller>', SellerProductList.as_view(), name='seller_product_list'),
+    path('<uuid:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('<seller>/', SellerProductList.as_view(), name='seller_product_list'),
 ]
