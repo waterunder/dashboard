b183a1aca1121bae4436d2f66d5493ad852bf839
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-04 23:09:15 -0300

    product create url added

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import ProductDetailView, ProductListView, SellerProductList
+from .views import (ProductCreate, ProductDetailView, ProductListView,
+                    SellerProductList)
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('<uuid:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('create/', ProductCreate.as_view(), name='product_create'),
     path('<seller>/', SellerProductList.as_view(), name='seller_product_list'),
 ]
