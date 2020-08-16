2f0c0589d877dccd50eb16ca84e09fbf387e77b9
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-16 18:43:11 -0300

    added logic to search products from query

--- products/urls.py
+++ products/urls.py
@@ -6,11 +6,13 @@ from products.views import (
     ProductDetailView,
     ProductListView,
     ProductUpdate,
+    SearchResultsListView,
     SellerProductList,
 )
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
+    path('search/', SearchResultsListView.as_view(), name='search_results'),
     path('<uuid:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('create/', ProductCreate.as_view(), name='product_create'),
     path('<uuid:pk>/update/', ProductUpdate.as_view(), name='product_update'),
