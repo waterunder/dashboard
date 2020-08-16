104c1a997c3e039228ec3799b6f65ab55ef79c5f
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-16 15:29:06 -0300

    added seller update and delete view

--- sellers/urls.py
+++ sellers/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 
-from .views import SellerCreate, SellerDetailView, SellerListView
+from .views import (
+    SellerCreate,
+    SellerDelete,
+    SellerDetailView,
+    SellerListView,
+    SellerUpdate,
+)
 
 urlpatterns = [
     path('', SellerListView.as_view(), name='seller_list'),
     path('<uuid:pk>', SellerDetailView.as_view(), name='seller_detail'),
     path('create/', SellerCreate.as_view(), name='seller_create'),
+    path('<uuid:pk>/update/', SellerUpdate.as_view(), name='seller_update'),
+    path('<uuid:pk>/delete/', SellerDelete.as_view(), name='seller_delete'),
 ]
