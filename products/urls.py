e3ec6fec71ae9856df597edba139e66d174af4e7
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-16 11:19:59 -0300

    modified url endpoint fo rupdate

--- products/urls.py
+++ products/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 
-from products.views import (ProductCreate, ProductDelete, ProductDetailView,
-                            ProductListView, ProductUpdate, SellerProductList)
+from products.views import (
+    ProductCreate,
+    ProductDelete,
+    ProductDetailView,
+    ProductListView,
+    ProductUpdate,
+    SellerProductList,
+)
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('<uuid:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('create/', ProductCreate.as_view(), name='product_create'),
-    path('<uuid:pk>/', ProductUpdate.as_view(), name='product_update'),
+    path('<uuid:pk>/update/', ProductUpdate.as_view(), name='product_update'),
     path('<uuid:pk>/delete/', ProductDelete.as_view(), name='product_delete'),
     path('<seller>/', SellerProductList.as_view(), name='seller_product_list'),
 ]
