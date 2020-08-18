7225c224a305f74469868846720ebca07bea550a
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-18 16:44:25 -0300

    updated for deployment

--- main/settings.py
+++ main/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.github',
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.facebook',
     'django_extensions',
 
     # Local
