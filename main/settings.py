9c4f5d9dae0e418e118d72787e89f95324317bb2
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-18 22:14:24 -0300

    removed github and added twitter

--- main/settings.py
+++ main/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'allauth.socialaccount.providers.github',
+    'allauth.socialaccount.providers.twitter',
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.facebook',
     'django_extensions',
