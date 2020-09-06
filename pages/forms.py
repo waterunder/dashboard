c510035afccf9b930dcfcfc00709d6b7333b727f
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-09-06 20:04:48 -0300

    added email field to contact form

--- pages/forms.py
+++ pages/forms.py
@@ -8,11 +8,20 @@ logger = logging.getLogger(__name__)
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Your name', max_length=100)
-    message = forms.CharField(label='Your message', max_length=600, widget=forms.Textarea)
+    name = forms.CharField(label='Name', max_length=100)
+    email = forms.EmailField(label='Email')
+    message = forms.CharField(label='Message', max_length=600, widget=forms.Textarea)
 
     def send_mail(self):
         logger.info('Sending email...')
-        message = 'From: {0}\n{1}'.format(self.cleaned_data['name'], self.cleaned_data['message'],)
-        send_mail(subject='Site message', message=message, from_email='site@website.domain',
-                  recipient_list=settings.RECIPIENT_LIST, fail_silently=False)
+        message = 'From: {0}\n{1}\n{2}'.format(
+            self.cleaned_data['name'],
+            self.cleaned_data['email'],
+            self.cleaned_data['message']
+        )
+        logger.info('message: %s', message)
+        send_mail(subject='Site message',
+                  message=message,
+                  from_email='site@website.domain',
+                  recipient_list=settings.RECIPIENT_LIST,
+                  fail_silently=False)
