3816b0bb577867e2cb64135ae47f108486f1afe5
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-09-08 16:41:34 -0300

    corred view names for crud operations

--- dive/urls.py
+++ dive/urls.py
@@ -1,7 +1,10 @@
-from dive.views import DiveDetailView, DiveListView
+from dive.views import DiveCreate, DiveDelete, DiveDetailView, DiveListView, DiveUpdate
 from django.urls import path
 
 urlpatterns = [
     path('', DiveListView.as_view(), name='dive_list'),
     path('<uuid:pk>/', DiveDetailView.as_view(), name='dive_detail'),
+    path('create/', DiveCreate.as_view(), name='dive_create'),
+    path('<uuid:pk>/update/', DiveUpdate.as_view(), name='dive_update'),
+    path('<uuid:pk>/delete/', DiveDelete.as_view(), name='dive_delete'),
 ]
--- dive/views.py
+++ dive/views.py
@@ -1,6 +1,14 @@
+import logging
+
 from dive.models import Dive
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
 from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+
+logger = logging.getLogger(__name__)
 
 
 class DiveListView(LoginRequiredMixin, ListView):
@@ -13,3 +21,29 @@ class DiveDetailView(LoginRequiredMixin, DetailView):
     model = Dive
     context_object_name = 'dive'
     template_name = 'dive/dive_detail.html'
+
+
+class DiveCreate(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    model = Dive
+    fields = ['date', 'lat', 'lon', 'visibility', 'bottom_time', 'description']
+    success_message = "Dive was created successfully!"
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+
+
+class DiveUpdate(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    model = Dive
+    fields = ['date', 'lat', 'lon', 'visibility', 'bottom_time', 'description']
+    success_message = "Dive was updated successfully!"
+
+
+class DiveDelete(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
+    model = Dive
+    success_url = reverse_lazy('dive_list')
+    success_message = "Dive was deleted successfully!"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super(DiveDelete, self).delete(request, *args, **kwargs)
