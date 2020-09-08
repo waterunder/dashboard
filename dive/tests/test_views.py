50c92a451735c975a59ccdd802123b586dbf633e
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-09-08 17:27:50 -0300

    unit tests for crud operations on dive model

---
+++ dive/tests/test_views.py
@@ -0,0 +1,102 @@
+from dive.factories import DiveFactory, UserFactory
+from dive.views import DiveCreate, DiveDelete, DiveDetailView, DiveListView, DiveUpdate
+from django.test import TestCase
+from django.urls import resolve, reverse
+
+
+# Create your tests here.
+class DiveListTests(TestCase):
+    def test_dive_list_view_resolves_divelistview(self):
+        view = resolve(reverse('dive_list'))
+        self.assertEqual(view.func.__name__, DiveListView.as_view().__name__)
+
+    def test_dive_list_redirects_for_logged_out_user(self):
+        pass
+
+    def test_dive_list_works_for_logged_in_user(self):
+        pass
+
+    def test_dive_list_view_shows_dives_for_logged_in_user_only(self):
+        pass
+
+
+class DiveDetailTests(TestCase):
+    def setUp(self):
+        self.user = UserFactory()
+        self.dive = DiveFactory(created_by=self.user)
+
+    def test_dive_detail_view_resolves_divedetailview(self):
+        view = resolve(self.dive.get_absolute_url())
+        self.assertEqual(view.func.__name__, DiveDetailView.as_view().__name__)
+
+    def test_dive_detail_redirects_for_logged_out_user(self):
+        pass
+
+    def test_dive_detail_works_for_logged_in_user(self):
+        pass
+
+
+class DiveCreateTests(TestCase):
+    def setUp(self):
+        self.user = UserFactory()
+        self.dive = DiveFactory(created_by=self.user)
+
+    def test_dive_create_resolves_divecreateview(self):
+        view = resolve('dive_create')
+        self.assertEqual(view.func.__name__, DiveCreate.as_view().__name__)
+
+    def test_dive_create_redirects_for_logged_out_user(self):
+        pass
+
+    def test_dive_create_works_for_logged_in_user(self):
+        pass
+
+
+class DiveUpdateTests(TestCase):
+    def setup(self):
+        self.user = UserFactory()
+        self.dive = DiveFactory(created_by=self.user)
+
+    def test_dive_update_works_for_user_who_created_the_dive(self):
+        pass
+
+    def test_dive_update_redirects_for_user_who_did_not_create_the_dive(self):
+        pass
+
+    def test_dive_update_works_for_super_user(self):
+        pass
+
+    def test_dive_update_resolves_diveupdateview(self):
+        view = resolve(self.dive.get_update_url())
+        self.assertEqual(view.func.__name__, DiveUpdate.as_view().__name__)
+
+    def test_dive_update_redirects_for_logged_out_user(self):
+        pass
+
+    def test_dive_update_works_for_logged_in_user(self):
+        pass
+
+
+class DiveDeleteTests(TestCase):
+    def setUp(self):
+        self.user = UserFactory()
+        self.dive = DiveFactory(created_by=self.user)
+
+    def test_dive_delete_works_for_logged_in_user(self):
+        pass
+
+    def test_dive_delete_redirects_for_logged_out_user(self):
+        pass
+
+    def test_dive_delete_works_for_super_user(self):
+        pass
+
+    def test_dive_delete_works_for_user_who_created_the_dive(self):
+        pass
+
+    def test_dive_delete_redirects_for_user_who_did_not_create_the_dive(self):
+        pass
+
+    def test_dive_delete_resolves_divedeleteview(self):
+        view = resolve(self.dive.get_delete_url())
+        self.assertEqual(view.func.__name__, DiveDelete.as_view().__name__)
