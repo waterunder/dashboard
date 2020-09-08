716b9eb34be2e1ae55ce0f187cfe9e6e80b372aa
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-09-08 16:41:10 -0300

    added model helper methods for absolute_url

--- dive/models.py
+++ dive/models.py
@@ -2,6 +2,7 @@ import uuid
 
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
 
 
 class Dive(models.Model):
@@ -18,3 +19,12 @@ class Dive(models.Model):
 
     def __str__(self):
         return str(self.date)
+
+    def get_absolute_url(self):
+        return reverse('dive_detail', args=[str(self.id)])
+
+    def get_update_url(self):
+        return reverse('dive_update', args=[str(self.id)])
+
+    def get_delete_url(self):
+        return reverse('dive_delete', args=[str(self.id)])
