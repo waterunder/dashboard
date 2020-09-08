fde5775f91c259216d65b1daa94a728464145d6d
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-09-08 14:48:49 -0300

    basic model structure for log book

--- dive/models.py
+++ dive/models.py
@@ -1,8 +1,20 @@
 import uuid
 
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
 class Dive(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    dive_date = models.DateField()
+    date = models.DateField()
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    lon = models.DecimalField(max_digits=9, decimal_places=6)
+    visibility = models.IntegerField()
+    bottom_time = models.IntegerField()
+    description = models.CharField(max_length=200, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.date)
