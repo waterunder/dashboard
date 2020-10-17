f66863fc481de9357be0e33516b02ee6465bc324
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-10-17 12:01:20 -0300

    spruced up the admin interface

--- sellers/admin.py
+++ sellers/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-
 from products.models import Product
 
 from .models import Seller
@@ -11,9 +10,13 @@ class ProductInline(admin.TabularInline):
 
 class SellerAdmin(admin.ModelAdmin):
     inlines = (ProductInline, )
-    list_display = ('name', 'email', 'country', 'is_active', 'owner',)
-    list_filter = ('name', 'country',)
+    list_display = ('name', 'email', 'city', 'zip_code', 'country', 'is_active', 'owner', 'created_at',)
+    list_filter = ('is_active', 'created_at', 'country',)
     list_editable = ('is_active',)
+    search_fields = ('name', 'email',)
+    raw_id_fields = ('owner',)
+    date_heirarchy = 'created_at'
+    ordering = ('-is_active', 'created_at',)
 
 
 admin.site.register(Seller, SellerAdmin)
