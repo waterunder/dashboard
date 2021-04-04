35885ab63df39b3e8382531c75645f67fbc637e7
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-04-04 12:01:41 -0300

    removed unused product tag admin

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.html import format_html
 
-from .models import Product, ProductImage, ProductTag, Review
+from .models import Product, ProductImage, Review
 
 
 class ReviewInline(admin.TabularInline):
@@ -24,16 +24,7 @@ class ProductAdmin(admin.ModelAdmin):
         return obj.seller.name
 
 
-class ProductTagAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug', 'active')
-    list_editable = ('active',)
-    list_filter = ('active',)
-    search_fields = ('name',)
-    prepopulated_fields = {"slug": ("name",)}
-
-
 admin.site.register(Product, ProductAdmin)
-admin.site.register(ProductTag, ProductTagAdmin)
 
 
 class ProductImageAdmin(admin.ModelAdmin):
